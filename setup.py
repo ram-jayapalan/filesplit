c4286d4271025169622dd7986a2c6e84701b0991
Ram Jayapalan <ramp16888@gmail.com>
2017-11-05 18:49:28 -0500

    updated setup.py file version to point to unstable release

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="filesplit",
     packages=["package"],
-    version=1.0,
+    version="v1.0-unstable",
     description="Module to split file of any size into multiple chunks",
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/master.zip",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/v1.0-unstable.tar.gz",
     keywords=["python", "python3", "file split", "split file", "file split"],
     classifiers=[]
 )
