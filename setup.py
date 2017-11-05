f50b57c7eb34c60f222374da39d6e71e988ca21e
Ram Jayapalan <ramp16888@gmail.com>
2017-11-05 18:37:22 -0500

    unstable package release

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup
+
+setup(
+    name="filesplit",
+    packages=["package"],
+    version=1.0,
+    description="Module to split file of any size into multiple chunks",
+    author="Ram Prakash Jayapalan",
+    author_email="ramp16888@gmail.com",
+    url="https://github.com/ram-jayapalan/filesplit",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/master.zip",
+    keywords=["python", "python3", "file split", "split file", "file split"],
+    classifiers=[]
+)
+
