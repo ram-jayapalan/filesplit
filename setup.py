36a0685b7e5feb02d4385860153f762d88377da9
Ram Jayapalan <ramp16888@gmail.com>
2017-11-05 22:55:05 -0500

    updated version naming convention

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="filesplit",
     packages=["package"],
-    version="1.0",
+    version="1.0.0",
     description="Module to split file of any size into multiple chunks",
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.0.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[]
 )
