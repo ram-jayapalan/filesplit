444464a57bb0223fb5ba38d96a04d4e15676fc1b
Ram Jayapalan <ramp16888@gmail.com>
2017-11-06 13:48:33 -0500

    bumped version to 1.0.2

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="filesplit",
     packages=["fsplit"],
-    version="1.0.1",
+    version="1.0.2",
     description="Module to split file of any size into multiple chunks",
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.1.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.2.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[]
 )
