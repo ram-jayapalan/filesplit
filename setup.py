215d3bb5be493d7470494c0ee6fb055dd60bfa30
Ram Jayapalan <ramp16888@gmail.com>
2017-11-05 22:48:37 -0500

    updated keywords in the setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
     download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.tar.gz",
-    keywords=["python", "python3", "file split", "split file", "file split"],
+    keywords="file split filesplit splitfile chunks splits",
     classifiers=[]
 )
 
