f4e769be73be63a27a7048c62a8755bc8a03ab70
Ram Jayapalan <ramp16888@gmail.com>
2017-11-06 15:04:10 -0500

    long desc specified

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 
 from distutils.core import setup
 
+
+def get_log_desc():
+
+    with open('README.rst') as f:
+        desc = f.read()
+
+    return desc
+
+
 setup(
     name="filesplit",
     packages=["fsplit"],
     version="1.0.2",
     description="Module to split file of any size into multiple chunks",
+    long_description=get_log_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
     download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.2.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[],
-    setup_cfg=True
 )
 
