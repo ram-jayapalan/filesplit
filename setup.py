b057e6c32b92fc78f0b24fcdd8492a6409cfac35
Ram Jayapalan <ramp16888@gmail.com>
2017-11-06 15:08:20 -0500

    bumped version to 1.0.3

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ def get_log_desc():
 setup(
     name="filesplit",
     packages=["fsplit"],
-    version="1.0.2",
+    version="1.0.3",
     description="Module to split file of any size into multiple chunks",
     long_description=get_log_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.2.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.3.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[],
 )
