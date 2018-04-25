620d55ce511b5b2fd926d435f95e0291a009a144
Ram Jayapalan <ramp16888@gmail.com>
2018-04-25 14:05:31 -0400

    apply readme not found fix

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ def get_long_desc():
 setup(
     name="filesplit",
     packages=["fsplit"],
-    version="1.0.4beta",
+    version="1.0.4",
     description="Module to split file of any size into multiple chunks",
     long_description=get_long_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.4beta.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.4.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[],
 )
