785c0169b38a0983cb48d4081e74cfbeb039306d
Ram Jayapalan <ramp16888@gmail.com>
2018-04-25 13:40:06 -0400

    apply readme not found fix

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 
 from distutils.core import setup
+import os
 
 
-def get_log_desc():
+def get_long_desc():
+
+    if not os.path.isfile('README.rst'):
+        return
 
     with open('README.rst') as f:
         desc = f.read()
@@ -15,11 +19,11 @@ setup(
     packages=["fsplit"],
     version="1.0.3",
     description="Module to split file of any size into multiple chunks",
-    long_description=get_log_desc(),
+    long_description=get_long_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.3.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/1.0.4.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[],
 )
