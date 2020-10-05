d55a4940f9d7490b1c9aa87c09e9f494f25706ea
Ram Jayapalan <ramp16888@gmail.com>
2020-10-05 11:23:51 -0400

    bump release in setup.py file

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ def get_long_desc():
 setup(
     name="filesplit",
     packages=["fsplit"],
-    version="2.0.0",
+    version="3.0.0",
     description="Module to split file of any size into multiple chunks",
     long_description=get_long_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
     download_url=
-    "https://github.com/ram-jayapalan/filesplit/archive/2.0.0.tar.gz",
+    "https://github.com/ram-jayapalan/filesplit/archive/v3.0.0.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[
         "Intended Audience :: Developers",
