591258a82e9b86e612e4d69bdc11ad0ad00b50ed
Ram Jayapalan <ramp16888@gmail.com>
2020-12-02 14:48:24 -0500

    update pypi release version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 import os
 
 
-def get_long_desc():
+def get_long_desc() -> str:
 
     if not os.path.isfile("README.rst"):
-        return
+        return ""
 
     with open("README.rst") as f:
         desc = f.read()
@@ -16,13 +16,13 @@ def get_long_desc():
 setup(
     name="filesplit",
     packages=["fsplit"],
-    version="3.0.1",
+    version="3.0.2",
     description="Module to split file of any size into multiple chunks",
     long_description=get_long_desc(),
     author="Ram Prakash Jayapalan",
     author_email="ramp16888@gmail.com",
     url="https://github.com/ram-jayapalan/filesplit",
-    download_url="https://github.com/ram-jayapalan/filesplit/archive/v3.0.1.tar.gz",
+    download_url="https://github.com/ram-jayapalan/filesplit/archive/v3.0.2.tar.gz",
     keywords="file split filesplit splitfile chunks splits",
     classifiers=[
         "Intended Audience :: Developers",
